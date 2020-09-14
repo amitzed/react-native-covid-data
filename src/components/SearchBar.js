7eb51d3307a23864310d99359b621fe1c31166ea
amitzed <amitzmn@gmail.com>
2020-09-14 17:11:07 -0400

    Serach input component initial styling complete

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.bgStyle}>
+      <FontAwesome name="search" style={styles.iconStyle} />
+      <TextInput placeholder="Search by Country" style={styles.inputStyle} />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  bgStyle: {
+    backgroundColor: '#FE9677',
+    height: 50,
+    marginHorizontal: 15,
+    borderRadius: 7,
+    flexDirection: 'row',
+    marginTop: 10
+  },
+  iconStyle: {
+    fontSize: 25,
+    marginHorizontal: 15,
+    alignSelf: 'center'
+  },
+  inputStyle: {
+    fontSize: 20,
+    flex: 1
+  }
+});
+
+export default SearchBar;
---
+++ src/views/SearchPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import SearchBar from '../components/SearchBar';
+
+const SearchPage = () => {
+  return (
+    <View>
+      <SearchBar />
+      <Text>SearchPage</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchPage;
